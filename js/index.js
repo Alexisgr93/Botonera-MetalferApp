e4a10173ba2311eadedb2c1ddc22a0b7a800eed2
Alexis Rodriguez <76778611+Alexisgr93@users.noreply.github.com>
2023-10-16 18:29:08 -0300

    Abrir en celular
    No esta actualizado desde el 2019 o posterior, varia mucho los tamaños y no esta al 100% para distintos tamaños

---
+++ js/index.js
@@ -0,0 +1,276 @@
+function playAudio1() { 
+
+var x = document.getElementById("myAudio1");
+
+       if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }
+
+ 
+
+
+function playAudio2() { 
+var x = document.getElementById("myAudio2");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    } 
+ 
+
+
+function playAudio3() { 
+var x = document.getElementById("myAudio3");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio4() { 
+var x = document.getElementById("myAudio4");
+   if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio5() { 
+var x = document.getElementById("myAudio5");
+ if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio6() { 
+var x = document.getElementById("myAudio6");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio7() { 
+var x = document.getElementById("myAudio7");
+   if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio8() { 
+var x = document.getElementById("myAudio8");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    } 
+
+
+function playAudio9() { 
+var x = document.getElementById("myAudio9");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    } 
+
+
+function playAudio10() { 
+var x = document.getElementById("myAudio10");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio11() { 
+var x = document.getElementById("myAudio11");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    } 
+
+
+function playAudio12() { 
+var x = document.getElementById("myAudio12");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio13() { 
+var x = document.getElementById("myAudio13");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio14() { 
+var x = document.getElementById("myAudio14");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    } 
+
+
+function playAudio15() { 
+var x = document.getElementById("myAudio15");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio16() { 
+var x = document.getElementById("myAudio16");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    } 
+
+
+function playAudio17() { 
+var x = document.getElementById("myAudio17");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio18() { 
+var x = document.getElementById("myAudio18");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    } 
+
+
+function playAudio19() { 
+var x = document.getElementById("myAudio19");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio20() { 
+var x = document.getElementById("myAudio20");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function playAudio21() { 
+var x = document.getElementById("myAudio21");
+    if (x.paused) {
+          x.play();
+       } else {
+          x.pause();
+		  x.currentTime = 0;
+       } 
+    }  
+
+
+function texto_aleatorio(){
+		var textos = new Array()
+		textos[0] = "Alexis Rodríguez: \n \n No queres saber."
+		textos[1] = "Alexis Rodríguez: \n \n Si Ya esta."
+		textos[2] = "Alexis Rodríguez: \n \n Si supuestamente."
+		textos[3] = "Eduardo Carpentieri: \n \n Tengo la mano prohibida."
+		textos[4] = "Eduardo Carpentieri: \n \n Vamos con el mio que es más rápido."
+		textos[5] = "Eduardo Carpentieri: \n \n Que flaquito que sos, como haces para coj… a tu mujer?"
+		textos[6] = "Eduardo Carpentieri: \n \n Me tocas la cara y te mando a enfriar."
+		textos[7] = "Eduardo Carpentieri: \n \n Si, si, si ya  esta."
+		textos[8] = "Eduardo Carpentieri: \n \n No, no, no, no, no."
+		textos[9] = "Eduardo Carpentieri: \n \n Te quedo el culito planchado??"
+		textos[10] = "Francisco Lugo: \n \n Mandalo así, si no es pa´ la NASA ni pa´ TACHINT."
+		textos[11] = "Francisco Lugo: \n \n Ta que la pario."
+		textos[12] = "Eduardo Gonzalez: \n \n Eh, pibe vos no te equivocas."
+		textos[13] = "Eduardo Gonzalez: \n \n Eh, pibe vos no comes."
+		textos[14] = "Eduardo Gonzalez: \n \n Che padre."
+		textos[15] = "Horacio Alvarez: \n \n Herny."
+		textos[16] = "Horacio Alvarez: \n \n OMY."
+		textos[17] = "Horacio Alvarez: \n \n Me agarro una fiaca."
+		textos[18] = "Juan: \n \n No vez que me esta pegando."
+		textos[19] = "Juan: \n \n Me pone el codo."
+		textos[20] = "Laura: \n \n Boludos Sobran."
+		textos[21] = "Néstor Bartoletti: \n \n Calidad Comercial."
+		textos[22] = "Néstor Bartoletti: \n \n Sabes lo que le haces falta a ésta."
+		textos[23] = "Néstor Bartoletti: \n \n Bochi, Bocha, Chicho, Bichi, Piri, Bebé."
+		textos[24] = "Omar Acuña: \n \n Nunca hiciste la del Payasito?"
+		textos[25] = "Raúl Orosco: \n \n Que haceeee Pipi!!"
+		textos[26] = "Raúl Orosco: \n \n Que Rompistes??"
+		textos[27] = "Raúl Orosco: \n \n Che Despertate!!!"
+		
+		aleat = Math.random() * (textos.length)
+		aleat = Math.floor(aleat)
+		window.alert(textos[aleat])
+	}
+	
+	
+	
+	
+	
